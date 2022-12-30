89bf7fed9332ee8564b52deea7d1c65c30244342
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-12-30 14:40:20 +0530

    Update did.sh

--- did.sh
+++ did.sh
@@ -10,7 +10,7 @@ wget2 -q -c -O "$1" "$2"; \
 echo "$1 downloaded"; \
 echo "$1 uploading pixel"; \
 #curl -T "$1" -u :8d92bb15-fba1-45d3-9a48-8a754ff11a68 https://pixeldrain.com/api/file/; \
-curl https://pixeldrain.com/api/file/ -u:${api[$rand]} --upload-file "$1"; \
+curl -g https://pixeldrain.com/api/file/ -u:${api[$rand]} --upload-file "$1"; \
 echo "$1 uploaded pixel"; \
 #echo "$1 UPLOADING DRIVE"; \
 #rclone copy /app/"$1" new:/April/"$3"; \
