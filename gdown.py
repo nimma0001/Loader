96432f188b8f4e18fae9c41d0d6d06e9eba2e8f6
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-12-31 20:51:52 +0530

    Update gdown.py

--- gdown.py
+++ gdown.py
@@ -5,27 +5,29 @@ import subprocess
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import date
 
+# bot settings
+TOKEN="YOUR_TOKEN" 
+ID="YOUR_ID"
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-scope = ["https://www.googleapis.com/auth/drive"]
 
-
+# command parser
 parser = argparse.ArgumentParser(description='google rive upload and download')
 parser.add_argument("-l", "--links", help="Prints the supplied argument.", nargs='*')
 args = parser.parse_args()
 
 
+# gdrive settings
+scope = ["https://www.googleapis.com/auth/drive"]
 gauth = GoogleAuth()
 gauth.auth_method = 'service'
 gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
 drive = GoogleDrive(gauth)
 
-
-about = drive.GetAbout()
-
-print('Current user name:{}'.format(about['name']))
-
-all_links = {}
-
+# bot message
+MESSAGE = "Screenshot 👉\nPixeldrain 👇👇👇👇\n480p👉 480_r\n720p👉 720_r\n1080p👉 1080_r\n-----------------------------\n\nOffical Site 👇👇👇👇\n480p👉 480p_r\n720p👉 720p_r\n1080p👉 1080p_r\n\nPASSWORD 👉 https://t.me/c/1227529573/1207"
+all_link = {}
+# loop for handling update
 for arg in args.links:
     link = arg.split('=')[1]
     file = drive.CreateFile({'id': link})
@@ -34,12 +36,25 @@ for arg in args.links:
     file.GetContentFile(name, acknowledge_abuse=True)
     data = subprocess.check_output(['bash', 'it.sh', name, date.today().strftime('%d')])
     print(data)
-    all_links[name[:6]] = {480: '', 720: '', 1080: ''}
-    if '480p' in name:
-        all_links[name[:6]][480] = data
-    elif '720p' in name:
-        all_links[name[:6]][720] = data
-    elif '1080p' in name:
-        all_links[name[:6]][1080] = data
+    all_links[name[:6]] = MESSAGE
+    # if '480p' in name:
+    #     all_links[name[:6]][480] = data
+    # elif '720p' in name:
+    #     all_links[name[:6]][720] = data
+    # elif '1080p' in name:
+    #     all_links[name[:6]][1080] = data
     
-    print(all_links)
+    if '480p' in name:
+        all_links[name[:6]].replace('480_r', '')
+        all_links[name[:6]].replace('480p_r', '')
+    if '720p' in name:
+        all_links[name[:6]].replace('720_r', '')
+        all_links[name[:6]].replace('720p_r', '')
+    if '1080p' in name:
+        all_links[name[:6]].replace('1080_r', '')
+        all_links[name[:6]].replace('1080p_r', '')
+    print(all_links[name[:6]])
+
+# for name, link in all_link.items():
+#     curl -s -X POST $URL -d chat_id=$ID -d text=name
+#     curl -s -X POST $URL -d chat_id=$ID -d text=link
